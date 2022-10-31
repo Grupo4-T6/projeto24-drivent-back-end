f28f1704f44766ac9472ec3d3155af96f92d3371
tiagodlb <tigolimab223@gmail.com>
2022-10-31 16:01:14 -0300

    feature: adding new throws

--- src/services/payments-service/index.ts
+++ src/services/payments-service/index.ts
@@ -1,17 +1,21 @@
 import { notFoundError } from '@/errors';
+import enrollmentRepository from '@/repositories/enrollment-repository';
 import paymentRepository, { CreatePaymentParams } from '@/repositories/payment-repository';
 import { exclude } from '@/utils/prisma-utils';
 
 async function createOrUpdatePaymentWithUserId(params: CreatePaymentParams) {
   const payment = params;
 
-  const newPayment = await paymentRepository.upsert(params.userId, payment, exclude(payment, 'userId'));
+  const newPayment = await paymentRepository.upsert(params.enrollmentId, payment, exclude(payment, 'enrollmentId'));
 
-  await  paymentRepository.upsert(payment.userId, payment , newPayment);
+  await  paymentRepository.upsert(payment.enrollmentId, payment , newPayment);
 }
 
 async function getPayments(userId: number) {
-  const payment = await paymentRepository.findPayments(userId);
+  if(!userId) throw notFoundError();
+  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  if(!enrollment) throw notFoundError();
+  const payment = await paymentRepository.findPayments(enrollment.id);
   if(!payment) throw notFoundError();
   return payment;
 }
